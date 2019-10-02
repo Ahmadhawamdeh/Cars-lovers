d1d7b73b82e4f5ee7828e53dae06ad83c917d209
ahmadhawamdeh <ahmadhawamdeh313@gmail.com>
2019-10-03 00:10:37 +0300

    add

---
+++ cuffejava.js
@@ -0,0 +1,12 @@
+var cuffe = prompt("What is the best coffee producing state?");
+if(cuffe === "Brazil"){
+   confirm("correct answer");
+}else{
+   confirm("sorry try again");
+}
+var cuffe = prompt("How many cups of coffee do you drink per day?");
+if(cuffe === "18"){
+   alert("correct answer");
+}else{
+   alert("sorry try again");
+}
