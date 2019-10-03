a8cab6ab7af0d3488bc05119c7b0ef80d4c359d4
ahmadhawamdeh <ahmadhawamdeh313@gmail.com>
2019-10-03 03:24:44 +0300

    added

--- cuffejava.js
+++ cuffejava.js
@@ -1,27 +1,7 @@
-var cuffe = prompt("What is the best coffee producing state?");
-if(cuffe === "Brazil"){
-   confirm("correct answer");
-}else{
-   confirm("sorry try again");
+var cuffe = prompt("What is the best coffee in the world?");
+while (cuffe != "jordan" ){
+ cuffe = prompt("sorry enter the correct value :");
 }
-var cuffe2 = prompt("How many cups of coffee do you drink per day?");
-if(cuffe2 === "18"){
-   alert("correct answer");
-}else{
-   alert("sorry try again");
-}
-
-
-
-var cuffe3 = prompt("enter number equal 50 ?");
-
-while (cuffe3 !=50 ){
-   cuffe3 = prompt("sorry enter the correct number which is 50 :");
-}
-
-
-if(cuffe3 === "50"){
-   alert("correct answer");
-}else{
-   alert("sorry try again");
+if(cuffe === "jordan"){
+ alert("correct answer");
 }
